3ed1b91a52d6bdb95431aa834fb39b1356695a27
rish0303 <107429877+rish0303@users.noreply.github.com>
2022-08-06 12:08:23 -0700

    Implemented setPixel

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -18,7 +18,13 @@ namespace caveofprogramming
 
     void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
     {
+        uint8_t* pPixel = _pPixels.get();
 
+        pPixel += (y * 3) * _width + (x * 3);
+
+        pPixel[0] = blue;
+        pPixel[1] = green;
+        pPixel[2] = red;
     }
 
     bool Bitmap::write(string filename)
--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@ using namespace caveofprogramming;
 
 int main()
 {
-    Bitmap bitmap(800, 600);
+    int const WIDTH = 800;
+    int const HEIGHT = 600;
+    Bitmap bitmap(WIDTH, HEIGHT);
 
+    for(int i = 0; i < WIDTH; ++i)
+    {
+        for (int j = 0; j < HEIGHT; ++j)
+        {
+            bitmap.setPixel (i, j, 255, 0, 0);
+        }
+    }
+
+    //bitmap.setPixel (WIDTH / 2, HEIGHT / 2, 255, 255, 255);
     bitmap.write("test.bmp");
 
     cout << "Finished." << endl;
